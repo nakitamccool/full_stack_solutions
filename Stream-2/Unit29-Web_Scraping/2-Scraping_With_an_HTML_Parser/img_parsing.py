77c26b598bdea3ec9695df1248a5185b1eec7ed6
nakitamccool <nakita@codeinstitute.net>
2017-08-17 09:55:17 +0000

    Updated and tested img_parsing.py
    1. Replaced urllib2 with urllib.request
    2. Decoded urlopen()
    3. Updated syntax (print statements)
    4. Removed list creation (no longer needed)

--- Stream-2/Unit29-Web_Scraping/2-Scraping_With_an_HTML_Parser/img_parsing.py
+++ Stream-2/Unit29-Web_Scraping/2-Scraping_With_an_HTML_Parser/img_parsing.py
@@ -1,17 +1,25 @@
-from bs4 import BeautifulSoup
-from urllib2 import urlopen
+#!/usr/bin/env python3
 
+import os
+from bs4 import BeautifulSoup  # BeautifulSoup4 package
+import urllib.request
+
+# Grab the HTML from a web page just like we did
+# in the first example
 my_address = "http://www.irishtimes.com/"
-html_page = urlopen(my_address)
-html_text = html_page.read()
+
+# Open my_address, read page and decode from bytes to text
+with urllib.request.urlopen(my_address) as html_page:
+    html_text = html_page.read().decode("utf-8")
+
+# Pass the HTML to the BeautifulSoup constructor.
+# The second argument tells beautiful soup which parser to use
 soup = BeautifulSoup(html_text, "html.parser")
 
-# Create a list to store our images in
-images = []
-# Store the images in the list
+# Store the images in a list
 images = soup.find_all("img")
 
 # Iterate over 'images' list and
 # print the 'src' of each image
 for img in images:
-    print img['src']
+    print(img['src'])
