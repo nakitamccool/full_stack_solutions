f4aca45ba3848dc0c39eb7d91042269f1acde1bf
nakitamccool <nakita@codeinstitute.net>
2017-08-17 10:38:25 +0000

    Updated and tested regex_test and beautiful_soup_test
    1. Updated syntax (print statements)
    2. Decoded output of urlopen()
    3. Replaced urllib2 with urllib.request

--- Stream-2/Unit29-Web_Scraping/2-Scraping_With_an_HTML_Parser/beautiful_soup_test.py
+++ Stream-2/Unit29-Web_Scraping/2-Scraping_With_an_HTML_Parser/beautiful_soup_test.py
@@ -7,10 +7,10 @@ import urllib.request
 # Grab the HTML from a web page just like we did
 # in the first example
 my_address = "https://docs.python.org/3.4/whatsnew/3.4.html"
+
+# Open my_address, read page and decode from bytes to text
 with urllib.request.urlopen(my_address) as html_page:
     html_text = html_page.read().decode("utf-8")
-# html_page = urlopen(my_address)
-# html_text = html_page.read()
 
 # Pass the HTML to the BeautifulSoup constructor.
 # The second argument tells beautiful soup which parser to use
