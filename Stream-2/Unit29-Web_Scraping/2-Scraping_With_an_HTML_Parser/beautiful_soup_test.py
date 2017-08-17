53ee19cd3fb5fb714f9fb4c604f91786c80d60d9
nakitamccool <nakita@codeinstitute.net>
2017-08-17 09:18:05 +0000

    Updated and tested beautiful soup
    1. Decoded urlopen(my_address)
    2. Removed encoding already in play
    3. Installed bs4 and lxml
    4. Updated my_address url to python 3.4

--- Stream-2/Unit29-Web_Scraping/2-Scraping_With_an_HTML_Parser/beautiful_soup_test.py
+++ Stream-2/Unit29-Web_Scraping/2-Scraping_With_an_HTML_Parser/beautiful_soup_test.py
@@ -1,17 +1,21 @@
+#!/usr/bin/env python3
+
 import os
 from bs4 import BeautifulSoup  # BeautifulSoup4 package
-from urllib2 import urlopen
+import urllib.request
 
 # Grab the HTML from a web page just like we did
 # in the first example
-my_address = "https://docs.python.org/2/whatsnew/2.7.html"
-html_page = urlopen(my_address)
-html_text = html_page.read()
+my_address = "https://docs.python.org/3.4/whatsnew/3.4.html"
+with urllib.request.urlopen(my_address) as html_page:
+    html_text = html_page.read().decode("utf-8")
+# html_page = urlopen(my_address)
+# html_text = html_page.read()
 
 # Pass the HTML to the BeautifulSoup constructor.
 # The second argument tells beautiful soup which parser to use
 soup = BeautifulSoup(html_text, "lxml")
 
-result = soup.get_text().encode("utf-8")
+result = soup.get_text()
 text = os.linesep.join([s for s in result.splitlines() if s])
-print text
+print(text)
